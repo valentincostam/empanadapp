df78485318e17f05536d7a98ed34cd282e49e721
Valentín Costa <valentincostam@gmail.com>
2021-04-19 00:38:53 -0300

    feat: add description in header

--- src/App.svelte
+++ src/App.svelte
@@ -68,11 +68,18 @@
   h1 {
     text-align: center;
     font-weight: 900;
-    font-size: 2.4rem;
+    font-size: 2.6rem;
     color: var(--primary-color);
     letter-spacing: -.12rem;
     margin: 0;
-    margin-bottom: 1rem;
+  }
+
+  p {
+    color: var(--primary-color);
+    font-size: 1.1rem;
+    text-align: center;
+    margin: 0;
+    margin-bottom: var(--gap);
   }
 
   .button {
@@ -151,6 +158,7 @@
 <div class="container">
   <header>
     <h1>empanad.app</h1>
+    <p>Armá tu pedido de empanadas</p>
   </header>
 
   <main>
