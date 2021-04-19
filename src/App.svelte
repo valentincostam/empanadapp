7a795c2a7a3b2b960f2f8bc85a154086d66269cd
Valentín Costa <valentincostam@gmail.com>
2021-04-19 01:31:01 -0300

    chore: update add line button text

--- src/App.svelte
+++ src/App.svelte
@@ -187,7 +187,7 @@
         >
           <path d="M20 15h4.071v2h-4.071v4.071h-2v-4.071h-4.071v-2h4.071v-4.071h2v4.071zm-8 6h-12v-2h12v2zm0-4.024h-12v-2h12v2zm0-3.976h-12v-2h12v2zm12-4h-24v-2h24v2zm0-4h-24v-2h24v2z"/>
         </svg>
-        Agregar {$orderLines.length == 0 ? 'una' : 'otra'} variedad
+         {$orderLines.length == 0 ? 'Empezar pedido' : 'Agregar otra variedad'}
       </button>
     {/if}
 
