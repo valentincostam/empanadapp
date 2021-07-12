54e07b75cc34a4eddd7bc525af87ddaffd771848
Valentín Costa <valentincostam@gmail.com>
2021-07-11 23:30:46 -0300

    feat: add hash params

--- src/stores.js
+++ src/stores.js
@@ -1,17 +1,43 @@
 import { writable } from "svelte/store";
 
-const defaultOrderLines = [
+let initialOrderLines = [
   { id: 1, quantity: 4, flavor: "carne" },
   { id: 2, quantity: 1, flavor: "verdura y queso" },
   { id: 3, quantity: 1, flavor: "choclo" },
 ];
 
-const storedOrderLines = localStorage.orderLines
-  ? JSON.parse(localStorage.orderLines)
-  : null;
+const hashParams = new URLSearchParams(location.hash.substr(1));
 
-export const orderLines = writable(storedOrderLines || defaultOrderLines);
+const urlOrderLines = [...hashParams.entries()].map(([flavor, quantity]) => {
+  const amount = parseInt(quantity);
 
-orderLines.subscribe(
-  (value) => (localStorage.orderLines = JSON.stringify(value))
-);
+  return {
+    id: `${Date.now()}${Math.floor(Math.random() * 999999)}`,
+    quantity: Number.isInteger(amount) ? amount : 1,
+    flavor,
+  };
+});
+
+if (urlOrderLines.length) {
+  initialOrderLines = urlOrderLines;
+} else if (localStorage.orderLines) {
+  initialOrderLines = JSON.parse(localStorage.orderLines);
+}
+
+export const orderLines = writable(initialOrderLines);
+
+orderLines.subscribe((value) => {
+  const validOrderLines = value.filter(
+    ({ quantity, flavor }) => quantity > 0 && flavor.trim() != ""
+  );
+
+  localStorage.orderLines = JSON.stringify(validOrderLines);
+
+  const searchParams = new URLSearchParams();
+
+  validOrderLines.forEach(({ flavor, quantity }) =>
+    searchParams.set(flavor, quantity)
+  );
+
+  location.hash = searchParams.toString();
+});
